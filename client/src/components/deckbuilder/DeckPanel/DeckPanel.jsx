bef9923ab143b32ea2b6f5bbe39f30fc420f8d48
Bo Banducci <bobanducci90@gmail.com>
2025-07-02 00:56:41 -0500

    added clear deck button

--- client/src/components/deckbuilder/DeckPanel/DeckPanel.jsx
+++ client/src/components/deckbuilder/DeckPanel/DeckPanel.jsx
@@ -1,17 +1,36 @@
 import { useDeck } from '../../../context/DeckContext'
 import DeckList from '../DeckList/DeckList'
 import ExportButtons from '../ExportButtons/ExportButtons'
+import Button from '../../common/Button/Button'
+import audioService from '../../../services/audioService'
 import './DeckPanel.css'
 
 function DeckPanel({ gameName, deckieUrl }) {
-  const { totalCards } = useDeck()
+  const { totalCards, clearDeck } = useDeck()
+  
+  const handleClearDeck = async () => {
+    await audioService.initializeOnUserGesture()
+    audioService.playButtonClick()
+    clearDeck()
+  }
   
   return (
     <div className="deck-section">
       <div className="deck-header">
         <h3>My Deck</h3>
-        <div className="deck-count">
-          {totalCards} cards
+        <div className="deck-info">
+          <div className="deck-count">
+            {totalCards} cards
+          </div>
+          {totalCards > 0 && (
+            <Button 
+              onClick={handleClearDeck}
+              className="clear-deck-btn"
+              variant="secondary"
+            >
+              Clear Deck
+            </Button>
+          )}
         </div>
       </div>
       
